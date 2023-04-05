cc452d80cb0b7afd7ab210692beb0c390ec5fa45
Leanh <datletb789@gmail.com>
2023-04-05 23:08:57 +0700

    fix header

--- app/src/main/java/com/example/loginandsignup/image_generator_gpt.java
+++ app/src/main/java/com/example/loginandsignup/image_generator_gpt.java
@@ -73,7 +73,7 @@ public class image_generator_gpt extends AppCompatActivity {
         RequestBody requestBody = RequestBody.create(jsonObject.toString(), JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/images/generations")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer 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")
                 .post(requestBody)
                 .build();
 
