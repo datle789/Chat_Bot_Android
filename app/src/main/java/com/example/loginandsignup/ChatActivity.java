248b8b0a4ee74179ef13cf3d4875760d3fe30463
08 Nguyễn Gia Bảo <kevinbao15072002@gmail.com>
2023-04-11 10:35:59 +0700

    Updated GPT-Turbo-3.5 model and fixed timeout issue

--- app/src/main/java/com/example/loginandsignup/ChatActivity.java
+++ app/src/main/java/com/example/loginandsignup/ChatActivity.java
@@ -18,6 +18,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -37,7 +38,8 @@ public class ChatActivity extends AppCompatActivity {
     MessageAdapter messageAdapter;
     public static final MediaType JSON
             = MediaType.get("application/json; charset=utf-8");
-    OkHttpClient client = new OkHttpClient();
+    OkHttpClient client = new OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS).build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,18 +90,24 @@ public class ChatActivity extends AppCompatActivity {
 
         JSONObject jsonBody = new JSONObject();
         try {
-            jsonBody.put("model","text-davinci-003");
-            jsonBody.put("prompt",question);
-            jsonBody.put("max_tokens",4000);
-            jsonBody.put("temperature",0);
+//            jsonBody.put("model","text-davinci-003");
+//            jsonBody.put("prompt",question);
+//            jsonBody.put("max_tokens",4000);
+//            jsonBody.put("temperature",0);
+            jsonBody.put("model","gpt-3.5-turbo");
+            JSONArray messageARR = new JSONArray();
+            JSONObject obj = new JSONObject();
+            obj.put("role","user");
+            obj.put("content",question);
+            messageARR.put(obj);
+            jsonBody.put("messages",messageARR);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
-                .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer " +
-                        "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")
+                .url("https://api.openai.com/v1/chat/completions")
+                .header("Authorization","Bearer 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")
                 .post(body)
                 .build();
 
@@ -116,7 +124,8 @@ public class ChatActivity extends AppCompatActivity {
                     try {
                         jsonObject = new JSONObject(response.body().string());
                         JSONArray jsonArray = jsonObject.getJSONArray("choices");
-                        String result = jsonArray.getJSONObject(0).getString("text");
+                        String result = jsonArray.getJSONObject(0).
+                                getJSONObject("message").getString("content");//getString("text");
                         addResponse(result.trim());
                     } catch (JSONException e) {
                         e.printStackTrace();
